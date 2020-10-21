fa9cf8a852b7b9df95ca08cae6a4203d622b5a2c
AlexisShanYan <62886872+AlexisShanYan@users.noreply.github.com>
2020-10-21 08:05:49 +0200

    Add files via upload

--- Code-Socket/src/stream/ClientThread.java
+++ Code-Socket/src/stream/ClientThread.java
@@ -14,61 +14,100 @@ import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.LinkedList;
 
+/**
+ * <b>ClientThread est la classe qui a pour rôle d'être un thread permettant à chaque client de diffuser son message sur le chat.</b>
+ * 
+ * <p>Chaque client aura un seul thread associé. Ce dernier s'occupera de diffuser le message de son client associé à tous les autres clients</p>
+ * 
+ * <p>Pour de plus amples informations sur le thread s'occupant de la connexion des nouveaux clients, 
+ * 					voir la documentation sur la classe EchoServerMultiThreaded.</p>
+ * 
+ * @see EchoServerMultiThreaded
+ * 
+ * @author alexis
+ * @version 2.0
+ */
+
 public class ClientThread
 	extends Thread {
 	
+	/**
+	 * La liste des sockets côté serveur qui sont liés aux sockets côté client. 
+	 * 			C'est une liste pouvant être accédée par toutes les instances de ClientThread
+	 * 
+	 *  @see ClientThread#totalSocket
+	 */
+	
 	public static LinkedList<Socket> list=new LinkedList<Socket>();
+	
+	/**
+	 * Symbolise le nombre de sockets côté serveur étant connectées à une socket client. 
+	 * 			C'est un nombre pouvant être accédé par toutes les instances de ClientThread
+	 * 
+	 * @see ClientThread#list
+	 */
+	
 	public static int totalSocket = 0;
 	
-	private Socket clientSocket;
-	private String clientName;
+	/**
+	 * <p>La socket côté serveur formant un canal de communication avec une machine d'un client.</p>
+	 */
 	
-	ClientThread(Socket s, int numberSocket, String clientName) {
+	private Socket clientSocket;
+	
+	/**
+	 * Constructeur ClientThread
+	 * 
+	 * <p>Lors de la construction de l'objet, on ajoute à la liste de sockets celle passée en paramètre. 
+	 * 			On incrémente aussi le nombre de sockets contenus dans la liste.</p>
+	 * 
+	 * @param s
+	 * @param clientName
+	 * 
+	 * @see ClientThread#list
+	 * @see ClientThread#totalSocket
+	 */
+	
+	ClientThread(Socket s) {
 		this.clientSocket = s;
 		list.add(clientSocket);
 		totalSocket += 1;
-		this.clientName = clientName;
 	}
 
  	/**
-  	* receives a request from client then sends an echo to the client
-  	* @param clientSocket the client socket
+  	* Se charge de transmettre le message du client associé à tous les autres clients connectés au chat.
+  	* 
+  	* @see ClientThread#list
+  	* @see ClientThread#totalSocket
+  	* @see ClientThread#clientSocket
+  	* @see ClientThread#clientName
   	**/
+	
 	public void run() {
     	  try {
-       		
+    		  
     		BufferedReader socIn = null;
     		socIn = new BufferedReader(
     			new InputStreamReader(clientSocket.getInputStream()));
     		
-    		for (int i=0; i<totalSocket; i++) {
-  			  PrintStream socOut = new PrintStream(list.get(i).getOutputStream());
-  			  socOut.println(clientName + " est entré dans le chat !");
-  		  	}
-	       
-    	   Files.write(Paths.get("filename.txt"), (clientName + " est entré dans le chat !" + "\n").getBytes(), StandardOpenOption.APPEND);
-    		
     		while (true) {
     		  String line = socIn.readLine();
-    		  if (line.equals(".")) {
+    		  
+    		  if (line.contains("leaves the chat")) {
     			  list.remove(clientSocket);
 				  totalSocket -= 1;
-    			  
-    			  for (int i=0; i<totalSocket; i++) {
-    	  			  PrintStream socOut = new PrintStream(list.get(i).getOutputStream());
-    	  			  socOut.println(clientName + " vient de se déconnecter !");
-	  		  	  }
-    			  Files.write(Paths.get("filename.txt"), (clientName + " vient de se déconnecter !" + "\n").getBytes(), StandardOpenOption.APPEND);
-    			  break;
     		  }
     		  
     		  for (int i=0; i<totalSocket; i++) {
       			  PrintStream socOut = new PrintStream(list.get(i).getOutputStream());
-    			  socOut.println(clientName + " : " + line);
+    			  socOut.println(line);
     		  }
-		      Files.write(Paths.get("filename.txt"), (clientName + " : " + line + "\n").getBytes(), StandardOpenOption.APPEND);
+    		  
+		      Files.write(Paths.get("filename.txt"), (line + "\n").getBytes(), StandardOpenOption.APPEND);
     		}
-    	} catch (Exception e) {
+    	} catch (NullPointerException e) {
+        	System.out.println("A client has disconnected"); 
+        } catch (Exception e) {
         	System.err.println("Error in EchoServer:" + e); 
         }
      }
